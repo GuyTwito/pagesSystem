b809771d72f1895fe621235ad6d03f6ddd2766f6
Guy Twito <guyy18@gmail.com>
2020-08-19 19:06:04 +0300

    move files

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -2,9 +2,9 @@ import { ReactInVue } from "vuera";
 import lowercaseKeys from "lowercase-keys";
 
 const ReactComponents = {
-  helloReact: ReactInVue(require("./HelloReact").default),
-  helloVue: require("./HelloVue").default,
-  notFound: require("./NotFound").default,
+  helloReact: ReactInVue(require("./react_pages/HelloReact").default),
+  helloVue: require("./vue_pages/HelloVue").default,
+  notFound: require("./vue_pages/NotFound").default,
 };
 
 export const AdditionalAttributes = {

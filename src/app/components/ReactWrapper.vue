9bd76788dd294ac518360046c27b957341c82bbd
Guy Twito <guyy18@gmail.com>
2020-08-19 19:01:56 +0300

    page wrapper init

---
+++ src/app/components/ReactWrapper.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    ReactWrapper<br /><br />
+    <component v-bind:is="currentPageFixed" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import pagesManage from "../pages";
+
+@Component({
+  components: pagesManage,
+})
+export default class ReactWrapper extends Vue {
+  @Prop() readonly currentPage!: string;
+
+  get currentPageFixed() {
+    const pageName = this.currentPage.toLowerCase();
+    if (Object.keys(pagesManage).map((page) => page.toLowerCase()).indexOf(pageName) !== -1)
+      return pageName;
+    return "notfound";
+  }
+}
+</script>
+
+<style scoped lang="less"></style>

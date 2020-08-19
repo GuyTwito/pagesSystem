eeb2a74db029a693bd36e9ea3764ca8150ecd187
Guy Twito <guyy18@gmail.com>
2020-08-19 20:26:28 +0300

    use AdditionalAttributes

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -2,14 +2,21 @@ import { ReactInVue } from "vuera";
 import lowercaseKeys from "lowercase-keys";
 
 const ReactComponents = {
+  Home: require("./vue_pages/Home").default,
   helloReact: ReactInVue(require("./react_pages/HelloReact").default),
   helloVue: require("./vue_pages/HelloVue").default,
-  Home: require("./vue_pages/Home").default,
   notFound: require("./vue_pages/NotFound").default,
 };
 
-export const AdditionalAttributes = {
-  helloReact: {},
-};
+export const AdditionalAttributes = lowercaseKeys({
+  helloReact: {
+    name: "Hello React"
+  },
+  notFound: {
+    hide: true 
+  }
+  
+
+});
 
 export default lowercaseKeys(ReactComponents);
